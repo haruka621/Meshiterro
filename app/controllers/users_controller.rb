eb29e587ff475e7d199b45926c65dfb231ec0150
EC2 Default User <115263610+haruka621@users.noreply.github.com>
2022-12-21 03:06:43 +0000

    [Finish]Meshiterro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -7,16 +8,24 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :profile_image)
   end
 
-  private
-  
-  def user_params
-    params.require(:user).permit(:name, :profile_image)
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if( user_id != login_user_id)
+     redirect_to post_images_path
+    end
   end
 end
